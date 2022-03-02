338836ec0493a36111c5b02b1baa8bf5a5d531d8
karim <karim@DESKTOP-I8EFLU8>
2022-03-02 19:29:01 +0100

    change sizes

--- StandManagementProject/Login.Designer.cs
+++ StandManagementProject/Login.Designer.cs
@@ -29,10 +29,17 @@ namespace StandManagementProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(828, 457);
+            this.Name = "Login";
             this.Text = "Login";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
