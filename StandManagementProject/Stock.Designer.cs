aff187507c16413d3707534c09214a6193427e4a
karim <karim@DESKTOP-I8EFLU8>
2022-03-02 19:22:54 +0100

    Frames

--- StandManagementProject/Stock.Designer.cs
+++ StandManagementProject/Stock.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace StandManagementProject
 {
-    partial class Form1
+    partial class Stock
     {
         /// <summary>
         /// Required designer variable.
@@ -32,10 +32,9 @@ namespace StandManagementProject
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Stock";
         }
 
         #endregion
     }
 }
-
