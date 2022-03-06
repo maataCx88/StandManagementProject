ef561a50853f427173466366f1ec6029ccfeaa7e
MahdiCVBN41 <100801634+MahdiCVBN41@users.noreply.github.com>
2022-03-06 21:12:16 +0100

    Update storedb.sql

--- storedb.sql
+++ storedb.sql
@@ -637,5 +637,5 @@ alter proc show_facture_clt_byDate
 as begin
 select fc.id as 'N° bon',fc.montant as 'Total',fc.versé as 'Versé',fc.reste as 'Reste',fc.remise as 'Remise',(fc.reste-fc.remise) as 'Rendu',
 fc.date_fact as 'Date',c.nom+' '+c.prenom as 'Client',e.nom as 'Vendeur' 
-from facture_clt as fc,client as c , employee as e where (c.id=fc.id_c and e.id=fc.id_u) --and fc.date_fact between @date1 and @date2 
+from facture_clt as fc,client as c , employee as e where (c.id=fc.id_c and e.id=fc.id_u) and fc.date_fact between @date1 and @date2 
 end
